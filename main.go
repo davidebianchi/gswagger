5d8b8c0a0366c0d5e2a7f75eea2bbd7ab3a7798b
Davide Bianchi <bianchidavide12@gmail.com>
2021-01-27 00:29:57 +0100

    feat: add custom documentation path

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/ghodss/yaml"
@@ -19,24 +20,30 @@ var (
 )
 
 const (
-	// JSONDocumentationPath is the path of the swagger documentation in json format.
-	JSONDocumentationPath = "/documentation/json"
-	// YAMLDocumentationPath is the path of the swagger documentation in yaml format.
-	YAMLDocumentationPath = "/documentation/yaml"
-	defaultOpenapiVersion = "3.0.0"
+	// DefaultJSONDocumentationPath is the path of the swagger documentation in json format.
+	DefaultJSONDocumentationPath = "/documentation/json"
+	// DefaultYAMLDocumentationPath is the path of the swagger documentation in yaml format.
+	DefaultYAMLDocumentationPath = "/documentation/yaml"
+	defaultOpenapiVersion        = "3.0.0"
 )
 
 // Router handle the gorilla mux router and the swagger schema
 type Router struct {
-	router        *mux.Router
-	swaggerSchema *openapi3.Swagger
-	context       context.Context
+	router                *mux.Router
+	swaggerSchema         *openapi3.Swagger
+	context               context.Context
+	jsonDocumentationPath string
+	yamlDocumentationPath string
 }
 
 // Options to be passed to create the new router and swagger
 type Options struct {
 	Context context.Context
 	Openapi *openapi3.Swagger
+	// JSONDocumentationPath is the path exposed by json endpoint. Default to /documentation/json.
+	JSONDocumentationPath string
+	// YAMLDocumentationPath is the path exposed by yaml endpoint. Default to /documentation/yaml.
+	YAMLDocumentationPath string
 }
 
 // NewRouter generate new router with swagger. Default to OpenAPI 3.0.0
@@ -51,10 +58,28 @@ func NewRouter(router *mux.Router, options Options) (*Router, error) {
 		ctx = context.Background()
 	}
 
+	yamlDocumentationPath := DefaultYAMLDocumentationPath
+	if options.YAMLDocumentationPath != "" {
+		if err := isValidDocumentationPath(options.YAMLDocumentationPath); err != nil {
+			return nil, err
+		}
+		yamlDocumentationPath = options.YAMLDocumentationPath
+	}
+
+	jsonDocumentationPath := DefaultJSONDocumentationPath
+	if options.JSONDocumentationPath != "" {
+		if err := isValidDocumentationPath(options.JSONDocumentationPath); err != nil {
+			return nil, err
+		}
+		jsonDocumentationPath = options.JSONDocumentationPath
+	}
+
 	return &Router{
-		router:        router,
-		swaggerSchema: swagger,
-		context:       ctx,
+		router:                router,
+		swaggerSchema:         swagger,
+		context:               ctx,
+		yamlDocumentationPath: yamlDocumentationPath,
+		jsonDocumentationPath: jsonDocumentationPath,
 	}, nil
 }
 
@@ -93,7 +118,7 @@ func (r Router) GenerateAndExposeSwagger() error {
 	if err != nil {
 		return fmt.Errorf("%w json marshal: %s", ErrGenerateSwagger, err)
 	}
-	r.router.HandleFunc(JSONDocumentationPath, func(w http.ResponseWriter, req *http.Request) {
+	r.router.HandleFunc(r.jsonDocumentationPath, func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write(jsonSwagger)
@@ -103,11 +128,18 @@ func (r Router) GenerateAndExposeSwagger() error {
 	if err != nil {
 		return fmt.Errorf("%w yaml marshal: %s", ErrGenerateSwagger, err)
 	}
-	r.router.HandleFunc(YAMLDocumentationPath, func(w http.ResponseWriter, req *http.Request) {
+	r.router.HandleFunc(r.yamlDocumentationPath, func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
 		w.Write(yamlSwagger)
 	}).Methods(http.MethodGet)
 
+	return nil
+}
+
+func isValidDocumentationPath(path string) error {
+	if !strings.HasPrefix(path, "/") {
+		return fmt.Errorf("invalid path %s. Path should start with '/'", path)
+	}
 	return nil
 }
