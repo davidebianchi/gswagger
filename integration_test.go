d35432abaf51c218970b7a466b8cd71f5b285fdf
Davide Bianchi <bianchidavide12@gmail.com>
2021-11-10 11:19:12 +0100

    test: add integration test

--- integration_test.go
+++ integration_test.go
@@ -22,7 +22,7 @@ const (
 
 func TestIntegration(t *testing.T) {
 	t.Run("router works correctly", func(t *testing.T) {
-		muxRouter := setupSwagger(t)
+		muxRouter, _ := setupSwagger(t)
 
 		w := httptest.NewRecorder()
 		r := httptest.NewRequest(http.MethodGet, "/hello", nil)
@@ -33,6 +33,52 @@ func TestIntegration(t *testing.T) {
 
 		body := readBody(t, w.Result().Body)
 		require.Equal(t, "OK", body)
+
+		t.Run("and generate swagger", func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, swagger.DefaultJSONDocumentationPath, nil)
+
+			muxRouter.ServeHTTP(w, r)
+
+			require.Equal(t, http.StatusOK, w.Result().StatusCode)
+
+			body := readBody(t, w.Result().Body)
+			require.Equal(t, "{\"components\":{},\"info\":{\"title\":\"test swagger title\",\"version\":\"test swagger version\"},\"openapi\":\"3.0.0\",\"paths\":{\"/hello\":{\"get\":{\"responses\":{\"default\":{\"description\":\"\"}}}}}}", body)
+		})
+	})
+
+	t.Run("works correctly with subrouter - handles path prefix", func(t *testing.T) {
+		muxRouter, swaggerRouter := setupSwagger(t)
+
+		muxSubRouter := muxRouter.NewRoute().Subrouter()
+		subRouter, err := swaggerRouter.SubRouter(apirouter.NewGorillaMuxRouter(muxSubRouter), swagger.SubRouterOptions{
+			PathPrefix: "/prefix",
+		})
+		require.NoError(t, err)
+
+		subRouter.AddRoute(http.MethodGet, "/foo", okHandler, swagger.Definitions{})
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/hello", nil)
+
+		muxRouter.ServeHTTP(w, r)
+
+		require.Equal(t, http.StatusOK, w.Result().StatusCode)
+
+		body := readBody(t, w.Result().Body)
+		require.Equal(t, "OK", body)
+
+		t.Run("correctly call sub router", func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/prefix/foo", nil)
+
+			muxRouter.ServeHTTP(w, r)
+
+			require.Equal(t, http.StatusOK, w.Result().StatusCode)
+
+			body := readBody(t, w.Result().Body)
+			require.Equal(t, "OK", body)
+		})
 
 		t.Run("and generate swagger", func(t *testing.T) {
 			w := httptest.NewRecorder()
@@ -57,7 +103,7 @@ func readBody(t *testing.T, requestBody io.ReadCloser) string {
 	return string(body)
 }
 
-func setupSwagger(t *testing.T) *mux.Router {
+func setupSwagger(t *testing.T) (*mux.Router, *swagger.Router) {
 	t.Helper()
 
 	context := context.Background()
@@ -74,17 +120,18 @@ func setupSwagger(t *testing.T) *mux.Router {
 	})
 	require.NoError(t, err)
 
-	handler := func(w http.ResponseWriter, req *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`OK`))
-	}
 	operation := swagger.Operation{}
 
-	_, err = router.AddRawRoute(http.MethodGet, "/hello", handler, operation)
+	_, err = router.AddRawRoute(http.MethodGet, "/hello", okHandler, operation)
 	require.NoError(t, err)
 
 	err = router.GenerateAndExposeSwagger()
 	require.NoError(t, err)
 
-	return muxRouter
+	return muxRouter, router
+}
+
+func okHandler(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`OK`))
 }
